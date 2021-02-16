314b8b63ecff27b5c254453710ad1e6b8d345ed2
santosbpm <santosbpm.0@gmail.com>
2021-02-15 22:16:52 -0300

    Codigo de teste dos algoritmos de ordenacao

---
+++ sorting/test.py
@@ -0,0 +1,32 @@
+''' 
+    Código de teste retirado do canal Programação Dinâmico (Python Café)
+    GitHub do código: https://github.com/python-cafe/algorithms.git
+'''
+import random
+from sorting import insertion_sort
+
+
+any_numbers = random.sample(range(1, 1000), 42)
+
+already_sorted = [1, 2, 3, 4, 5, 6, 9, 20, 22, 23, 28, 
+                    32, 34, 39, 40, 42, 76, 87, 99, 112]
+
+inversed = [117, 90, 88, 83, 81, 77, 74, 69, 64, 63, 51,
+            50, 49, 42, 41, 34, 32, 29, 28, 22, 16, 8, 6, 5, 3, 1]
+
+repeated = [7, 7, 7, 7, 7, 1, 1, 9, 9, 0, 4, 4, 4, 5, 4, 5, 7, 1,]
+
+if __name__ == "__main__":
+    test_cases = {'Números aleatórios': any_numbers, 
+                    'Já ordenados': already_sorted, 
+                    'Ordem inversa': inversed, 
+                    'Elementos repetidos': repeated
+                }
+    print("*******************************")
+    for name, arr in test_cases.items():
+        print(f"\nCaso de teste: {name}"
+        print(arr)
+        insertion_sort(arr)
+        print("\n Ordenado:")
+        print(arr)
+    print("*******************************")

6b7119eb4ef5c1ea38b916ec8e5629b289a32b0e
santosbpm <santosbpm.0@gmail.com>
2021-02-21 00:36:39 -0300

    Adicionando algoritmo duplicate_elements e analisando sua complexidade de tempo

--- algorithms.py
+++ algorithms.py
@@ -54,4 +54,20 @@ def reverse_list(arr):
         arr[size-i-1] = aux
 
 # 4 + N complexidade de tamanho
-# 2 + 4*(N/2) == 2 + 2*N complexidade de tempo
+# 2 + 4*(N/2) == 2 + 2*N complexidade de tempo = O(N)
+
+
+# Procura por elementos duplicados
+def duplicate_elements(arr):
+    for i in range(len(arr)-1):
+        for j in  range(i+1, len(arr)-1):
+            if arr[i] == arr[j]:
+                return True
+    return False
+
+# Complexidade de Tempo
+''' N-1 + N-2 + N-3 +... =       
+    N * (N - 1) / 2 + 1 =
+    (N ^ 2 - N) / 2 + 1 =
+    O(N^2)
+'''

9ca76fb25144a14bc7651ef240d7319946323b4a
santosbpm <santosbpm.0@gmail.com>
2021-02-27 00:19:06 -0300

    organizando diretorios e separando funcoes dos algoritmo.py renomeado para ordenacao.py

--- ordenacoes/ordenacao.py
+++ ordenacoes/ordenacao.py
@@ -55,30 +55,3 @@ def reverse_list(arr):
 
 # 4 + N complexidade de tamanho
 # 2 + 4*(N/2) == 2 + 2*N complexidade de tempo = O(N)
-
-
-# Procura por elementos duplicados
-def duplicate_elements(arr):
-    for i in range(len(arr)-1):
-        for j in  range(i+1, len(arr)-1):
-            if arr[i] == arr[j]:
-                return True
-    return False
-
-# Complexidade de Tempo
-''' N-1 + N-2 + N-3 +... =       
-    N * (N - 1) / 2 + 1 =
-    (N ^ 2 - N) / 2 + 1 =
-    O(N^2)
-'''
-
-
-# Busca Linear em Lista de Alocação Sequencial
-
-def search(arr, elem):
-    ''' Retorna o indice do elemento se ele estiver na lista,
-        ou None, caso não esteja '''
-    for i in range(len(arr)):
-        if arr[i] == elem:
-            return i
-    return None
